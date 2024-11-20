51193569ebef92232a3cc84f98192b7f19d108f3
nparedes <nparedes@alltic.com.co>
2024-11-20 08:52:56 -0500

    Se ajusta shopSlice

--- src/features/shop/shopSlice.js
+++ src/features/shop/shopSlice.js
@@ -1,26 +1,18 @@
 import { createSlice } from "@reduxjs/toolkit";
-import categories from '../../data/categories.json'
-import products from '../../data/products.json'
 
 export const shopSlice = createSlice({
     name: 'shop',
     initialState: {
         value: {
-            categories: categories,
-            products: products,
             categorySelected:'',
-            productsFiltered: [],
             productSelected: null,
-            productShown: []
         }
     },
     reducers: {
         setCategory : (state,action) => {
-            state.value.productsFiltered = products.filter (product => product.category.toLocaleLowerCase() === action.payload.toLocaleLowerCase())
             state.value.categorySelected = action.payload
         },
         setProduct : (state,action) => {
-            state.value.productShown = products.filter (product => product.id === action.payload)
             state.value.productSelected = action.payload
         }
     }
